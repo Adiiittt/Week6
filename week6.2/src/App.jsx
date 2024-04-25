2688c704a1cb822543564712ae435f903b7a5bf3
Adiiittt <avyas8927@gmail.com>
2024-04-25 19:35:55 +0530

    use Memo Start

--- week6.2/src/App.jsx
+++ week6.2/src/App.jsx
@@ -1,64 +1,26 @@
 import { useState } from "react";
-import { useEffect } from "react";
-import axios from "axios";
 
 function App() {
-  // state? useState
-  const [selectedId, setSelectedId] = useState(1);
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
-  return (
-    <div>
-      <button onClick={() => setSelectedId(1)}>1</button>
 
-      <button
-        onClick={function () {
-          setSelectedId(2);
-        }}
-      >
-        2
-      </button>
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count = count + i;
+  }
 
-      <button
-        onClick={function () {
-          setSelectedId(3);
-        }}
-      >
-        3
-      </button>
-      <button
-        onClick={function () {
-          setSelectedId(4);
-        }}
-      >
-        4
-      </button>
-
-      <Todo id={selectedId} />
-    </div>
-  );
-}
-
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-
-  // implement effect here
-  useEffect(() => {
-    setTimeout(() => {
-      axios
-        .get(`https://sum-server.100xdevs.com/todo?id=${id}`)
-        .then((response) => {
-          setTodo(response.data.todo);
-        });
-    }, 100);
-  }, [id]);
-
-  return (
-    <div>
-      Id: {id}
-      <h1>{todo.title}</h1>
-      <h4>{todo.description}</h4>
-    </div>
-  );
+  return <div>
+    <input onChange={function(e) {
+      setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br />
+    Sum from 1 to {inputValue} is {count}
+    <br />
+    <button onClick={() => {
+      setCounter(counter + 1);
+    }}>Counter ({counter})</button>
+  </div>
 }
 
 export default App;
